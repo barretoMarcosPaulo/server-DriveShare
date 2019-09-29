9b467018a9dc846913da45c50a4be7b9e399a949
barretoMarcosPaulo <marcospaulo@ifpi.edu.br>
2019-09-29 17:05:53 -0300

    update: in loop await connection

--- server.py
+++ server.py
@@ -17,44 +17,38 @@ class ServerSide():
 	def startServer(self):
 		self.server_socket.bind(self.addr)		
 		self.server_socket.listen(10)
-		print('Await Connection...')
-		self.connection,self.client = self.server_socket.accept()
-		print('Connected , Client = ', self.client)
-		self.receiveDatas()
+		
+		while True:
+			print('Await Connection...')
+			self.connection,self.client = self.server_socket.accept()
+			print('Connected , Client = ', self.client)
+			self.receiveDatas()
 	
 	def stopServer(self):
 		self.server_socket.close()
 
 	def receiveDatas(self):
-		"""
-			Indexes
-			[0] - Operation
-			[1] - Name
-			[2] - LastName
-			[3] - Password
-			[4] - Email
 
-		"""
-		while True:
-			received = self.connection.recv(1024)
-			data_received = received.decode() 
-			data_received = data_received.split(',') 
-		    
-			isRegistred = "Ok"
+		received = self.connection.recv(1024)
+		data_received = received.decode() 
+		data_received = data_received.split(',') 
+	    
+		isRegistred = "Ok"
 
-			if data_received[0] == "Register":
-				if not self.MYSQL.email_is_regitred(data_received[4]):
-					self.MYSQL.save_datas(
-						data_received[1], 
-						data_received[2], 
-						data_received[4], 
-						data_received[3], 
-					)
-					self.connection.send(isRegistred.encode())
-				else:
-					isRegistred ="Error"
-					self.connection.send(isRegistred.encode())
-
+		if data_received[0] == "Register":
+			if not self.MYSQL.email_is_regitred(data_received[4]):
+				self.MYSQL.save_datas(
+					data_received[1], 
+					data_received[2], 
+					data_received[4], 
+					data_received[3], 
+				)
+				self.connection.send(isRegistred.encode())
+			else:
+				isRegistred ="Error"
+				self.connection.send(isRegistred.encode())
+		else:
+			print(data_received)
 
 driveShare = ServerSide()
 driveShare.startServer()
